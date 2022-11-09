f0c38e9ec377c9da134cb199eb46e7499b9a6567
winokush <winokush9@gmail.com>
2022-11-09 12:55:08 +0100

    Add page login

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    "./src/**/*.js",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '575px',
+        // => @media (min-width: 992px) { ... }
+      },
+      fontFamily: {
+        'Poppins': ['Poppins', ...defaultTheme.fontFamily.sans]
+      },
+      transitionProperty: {
+        'left': 'left',
+      }
+    },
+  },
+  plugins: [ 
+    require('@tailwindcss/forms') 
+  ],
+};
