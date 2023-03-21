f272d94af612ac23318d6bcd6c43b6714915f986
winboy <winokush9@gmail.com>
2023-03-21 17:42:53 +0100

    Add iso's needs

--- src/script.js
+++ src/script.js
@@ -129,6 +129,62 @@
 
         /***** ===== Filter Script End ===== *****/
 
+        /**
+         =================================================
+         Hidden/Display Some Part Script Begin
+         =================================================
+         +*/
+
+        /* ==== Parcours de soins Begin ==== */
+
+        $(".lc-new-consultation").click(function() {
+
+            $(".lc-parcours-form").slideDown("fast");
+
+            $(this).addClass("hidden");
+
+            $(".lc-consultation-container").addClass("hidden");
+
+        });
+
+        $(".lc-close-parcours-forms").click(function() {
+
+            $(".lc-parcours-form").attr("style", "display: none;");
+
+            $(".lc-new-consultation").removeClass("hidden");
+
+            $(".lc-consultation-container").removeClass("hidden");
+
+        });
+
+        /* ==== Parcours de soins End ==== */
+
+        /* ==== Bon Begin ==== */
+
+        $(".lc-new-invoice").click(function() {
+
+            $(".lc-bons-form").slideDown("fast");
+
+            $(this).addClass("hidden");
+
+            $(".lc-invoice-container").addClass("hidden");
+
+        });
+
+        $(".lc-close-bons-form").click(function() {
+
+            $(".lc-bons-form").attr("style", "display: none;");
+
+            $(".lc-new-invoice").removeClass("hidden");
+
+            $(".lc-invoice-container").removeClass("hidden");
+
+        });
+
+        /* ==== Bon End ==== */
+
+        /***** ===== Hidden/Display Some Part Script End ===== *****/
+
 
 
         /**
@@ -219,6 +275,107 @@
             });
 
         /***** ===== Profile Available Script End ===== *****/
+
+        /**
+         =================================================
+         Duplicate Item Script Begin
+         =================================================
+         +*/
+
+        $(document).on("click", "#addExam", function(e) {
+
+            const $targetLinePromo = $('.lc-exam-row');
+
+            var $lastRepeatingGroup = $targetLinePromo.last();
+
+            setTimeout(function() {
+
+                let $cloneLastTarget = $lastRepeatingGroup.clone(true);
+
+                $cloneLastTarget.find("input").val("");
+
+                $cloneLastTarget.insertAfter($lastRepeatingGroup);
+
+            }, 100);
+
+        });
+
+        $(".lc-exam-delete").click(function() {
+
+            if($('.lc-exam-row').length < 2) {
+                return;
+            }
+
+
+            $(this).closest(".lc-exam-row").remove();
+
+        });
+
+        /* ========================================================== */
+
+        $(document).on("click", "#addOrder", function(e) {
+
+            const $targetLinePromo = $('.lc-order-row');
+
+            var $lastRepeatingGroup = $targetLinePromo.last();
+
+            setTimeout(function() {
+
+                let $cloneLastTarget = $lastRepeatingGroup.clone(true);
+
+                $cloneLastTarget.find("input").val("");
+
+                $cloneLastTarget.insertAfter($lastRepeatingGroup);
+
+            }, 100);
+
+        });
+
+        $(".lc-order-delete").click(function() {
+
+            if($('.lc-order-row').length < 2) {
+                return;
+            }
+
+
+            $(this).closest(".lc-order-row").remove();
+
+        });
+
+        /* ========================================================== */
+
+        $(document).on("click", "#addBon", function(e) {
+
+            const $targetLinePromo = $('.lc-bon-row');
+
+            var $lastRepeatingGroup = $targetLinePromo.last();
+
+            setTimeout(function() {
+
+                let $cloneLastTarget = $lastRepeatingGroup.clone(true);
+
+                $cloneLastTarget.find("input").val("");
+
+                $cloneLastTarget.insertAfter($lastRepeatingGroup);
+
+            }, 100);
+
+        });
+
+        $(".lc-bon-delete").click(function() {
+
+            if($('.lc-bon-row').length < 2) {
+                return;
+            }
+
+
+            $(this).closest(".lc-bon-row").remove();
+
+        });
+
+        /***** ===== Duplicate Item Script End ===== *****/
+
+
 
         /**
          =================================================
@@ -321,6 +478,56 @@
         });
 
          /***** ===== Tabs Script End ===== *****/
+
+
+
+        /**
+         =================================================
+         Select 2 Script Begin
+         =================================================
+         +*/
+
+        $('.antecedent-familiaux').select2();
+
+        $('.antecedent-medicaux').select2();
+
+        $(".select2-selection--multiple").addClass("w-full text-slate-500 mt-1 py-2 border-none ring-1 ring-gray-200 focus:ring-gray-200 placeholder-slate-400 rounded-md");
+
+        $(".select2-selection--multiple").css("border", "none");
+
+        /***** ===== Select 2 Script End ===== *****/
+
+
+
+        /**
+         =================================================
+         Sweet Alert 2 Script Begin
+         =================================================
+         +*/
+
+        $(".sweetalert").click(function () {
+
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning', /* error | success */
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                    )
+                }
+            });
+
+        });
+
+        /***** ===== Sweet Alert 2 Script End ===== *****/
 
     });
 
